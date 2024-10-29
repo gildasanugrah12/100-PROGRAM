2fdf99b6ddc8f20d9062ab42ae391a7236c94508
= <=>
2024-10-29 10:34:47 +0700

    100PROGRAM

--- kerucut.py
+++ kerucut.py
@@ -2,10 +2,16 @@ print ("="*25)
 print ("\tkerucut")
 print ("="*25)
 
-phi = 3,14
+PHI = 3.14
+s = int(input('masukan nilai sisi\t: '))
 t = int(input('masukan nilai tinggi\t: '))
-r = (input('masukan nilai jari jari alas kerucut\t: '))
+r = int(input('masukan nilai jari jari alas kerucut\t: '))
 
-# mencari volume kerucut
-v = 1/3 * phi * r * t
-print ('hasil volume adalah\t: ' ,v)
+ls = PHI * r * s
+lp = (PHI * r * s) + (PHI * r ** 2)
+v = 1/3 * PHI * r **2 * t
+
+print ('LUAS KERUCUT\t\t:',ls)
+print ('LUAS PERMUKAAN\t\t:',lp)
+print ('VOLUME\t\t\t:',v)
+
